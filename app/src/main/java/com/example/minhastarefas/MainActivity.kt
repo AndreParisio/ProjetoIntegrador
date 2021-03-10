60d2a49a97b2fd377f88218ea326beef6219be57
Andre Parisio <andreparisio@gmail.com>
2021-03-10 17:04:57 -0300

    adicionado testado botao

--- app/src/main/java/com/example/minhastarefas/MainActivity.kt
+++ app/src/main/java/com/example/minhastarefas/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.minhastarefas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-val novaTela = Intent(this,Tela2::class.java)
-        startActivity(novaTela)
+        val botao: Button = findViewById(R.id.btnoutratela)
+        botao.setOnClickListener {
+            val novaTela = Intent(this, Tela2::class.java)
+            startActivity(novaTela)
+        }
     }
+
 }
